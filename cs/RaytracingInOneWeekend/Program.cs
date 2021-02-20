8aafb6c91c808c03427acebcc56cfdbd28761d81
Amit Netanel <netanel.amit@gmail.com>
2021-02-20 20:38:44 +0200

    producing gradient based on rays

--- cs/RaytracingInOneWeekend/Program.cs
+++ cs/RaytracingInOneWeekend/Program.cs
@@ -4,31 +4,81 @@ using System.IO;
 namespace RaytracingInOneWeekend
 {
     static class Program
-    {
+    {    
+        static readonly Vec3 UnitVector = new Vec3(1, 1, 1);
+        static readonly Vec3 ZeroVector = new Vec3(0, 0, 0);
+        static readonly Vec3 Background = new Vec3(0.5, 0.7, 1);
+        
+        static Vec3 RayColor(Ray ray)
+        {
+            var unitDirection = Vec3.UnitVector(ray.Direction);
+            
+            var t = 0.5 * (unitDirection.Y + 1);
+            
+            return (1.0 - t) * UnitVector + t * Background;
+        }
+
         static void Main()
         {
             var stdout = new StreamWriter(Console.OpenStandardOutput()) {AutoFlush = true};
             var stderr = new StreamWriter(Console.OpenStandardError()) {AutoFlush = true};
-            const int imageWidth = 256;
-            const int imageHeight = 256;
+            
+            //image:
+            var aspectRatio = 16.0 / 9.0;
+            var imageWidth = 400;
+            var imageHeight = imageWidth / aspectRatio;
+            
+            //camera:
+            
+            var viewportHeight = 2.0f;
+            var viewportWidth = aspectRatio * viewportHeight;
+            var focalLength = 1.0;
+            stderr.WriteLine($"{viewportWidth}, {viewportHeight}, {focalLength}");
+            
+            var origin = new Vec3(0, 0, 0);
+            stderr.WriteLine($"{origin.X}, {origin.Y}, {origin.Z}");
+            var horizontalVector = new Vec3(viewportWidth, 0, 0);
+            stderr.WriteLine($"{horizontalVector.X}, {horizontalVector.Y}, {horizontalVector.Z}");
+            var verticalVector = new Vec3(0, viewportHeight, 0);
+            stderr.WriteLine($"{verticalVector.X}, {verticalVector.Y}, {verticalVector.Z}");
+            var lowerLeftCorner = origin - horizontalVector/2.0 - verticalVector/2.0 - new Vec3(0, 0, focalLength);
+            stderr.WriteLine($"{lowerLeftCorner.X}, {lowerLeftCorner.Y}, {lowerLeftCorner.Z}");
+            
+            //render:
 
             stdout.WriteLine($"P3\n{imageWidth} {imageHeight}\n255\n");
 
-            for (int y = 0; y < imageHeight; y++)
+            for (int y = (int)imageHeight-1; y > 0; --y)
             {
                 stderr.Write($"Scanlines remaining: {y} \r");
                 for (int x = 0; x < imageWidth; x++)
                 {
-                    var r = (double) x / (imageWidth-1);
-                    var g = (double) y / (imageHeight-1);
-                    var b = 0.25f;
-
-                    var intR = (int) (255.99 * r);
-                    var intG = (int) (255.99 * g);
-                    var intB = (int) (255.99 * b);
+                    //// 1.0 gradients
+                    // var r = (double) x / (imageWidth-1);
+                    // var g = (double) y / (imageHeight-1);
+                    // var b = 0.25f;
+                    //
+                    // var intR = (int) (255.99 * r);
+                    // var intG = (int) (255.99 * g);
+                    // var intB = (int) (255.99 * b);
                     
-                    stdout.WriteLine($"{intR} {intG} {intB}");
+                    //stdout.WriteLine($"{intR} {intG} {intB}");
+                    
+                    // 2.0 ray gradients
+                    var u = (double) x / (imageWidth-1);
+                    var v = (double) y / (imageHeight-1);
+                    var ray = new Ray(origin, lowerLeftCorner + u*horizontalVector + v*verticalVector - origin);
+                    var color = RayColor(ray);
+                    WriteColor(stdout, color);
                 }    
+            }
+
+            static void WriteColor(StreamWriter stdout, Vec3 color)
+            {
+                var intR = (int) (255.99 * color.R);
+                var intG = (int) (255.99 * color.G);
+                var intB = (int) (255.99 * color.B);
+                stdout.WriteLine($"{intR} {intG} {intB}");
             }
             
             stdout.Close();
---
+++ cs/RaytracingInOneWeekend/Vec3.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace RaytracingInOneWeekend
+{
+    // Taken from https://github.com/ronnieholm/Ray-tracing-in-one-weekend-CSharp
+    // To be fair, I learned a lot from the 'cannon' implementation, and frankly this is way better implemented than what I originally did.
+    
+    // In C#, we cannot create a generic Vec3<T> where T could be int, long,
+    // float, or double. Using where to constrain T by design has no "where
+    // numeric type". It's only possible to create a Vec3<T> and dispatch on
+    // type at runtime. For some types this is okay, but not for a Vec3 on the
+    // hot path.
+    //
+    // To avoid heap allocation, Vec3 is implemented as a struct instead of a
+    // class. That way allocations happen on the stack instead. Turns out in
+    // .NET, there's negligible difference between float and double, so we went
+    // with double.
+    readonly struct Vec3
+    {
+        public double X { get; }
+        public double Y { get; }
+        public double Z { get; }
+
+        public double R => X;
+        public double G => Y;
+        public double B => Z;
+        
+        public Vec3(double e0, double e1, double e2)
+        {
+            X = e0;
+            Y = e1;
+            Z = e2;
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public double Length() => Math.Sqrt(X * X + Y * Y + Z * Z);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public double SquaredLength() => X * X + Y * Y + Z * Z;
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vec3 operator+(Vec3 v1, Vec3 v2) => new Vec3(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vec3 operator-(Vec3 v1, Vec3 v2) => new Vec3(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vec3 operator*(Vec3 v1, Vec3 v2) => new Vec3(v1.X * v2.X, v1.Y * v2.Y, v1.Z * v2.Z);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vec3 operator*(Vec3 v, double t) => new Vec3(v.X * t, v.Y * t, v.Z * t);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vec3 operator*(double t, Vec3 v) => new Vec3(v.X * t, v.Y * t, v.Z * t);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vec3 operator/(Vec3 v, double t) => new Vec3(v.X / t, v.Y / t, v.Z / t);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vec3 UnitVector(Vec3 v) => v / v.Length();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static double Dot(Vec3 v1, Vec3 v2) => v1.X * v2.X + v1.Y * v2.Y + v1.Z * v2.Z;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vec3 Cross(Vec3 v1, Vec3 v2) =>
+            new Vec3(v1.Y * v2.Z - v1.Z * v2.Y,
+                v1.Z * v2.X - v1.X * v2.Z,
+                v1.X * v2.Y - v1.Y * v2.X);
+    }
+}
